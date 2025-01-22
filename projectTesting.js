daea0e5a81a05628475f70b43342e57246d9b719
rohit <rohitjames77@outlook.com>
2025-01-22 18:39:19 +0530

    Add capitalize function

---
+++ projectTesting.js
@@ -0,0 +1,21 @@
+function capitalize(string) {
+  const firstCapLtr = string.slice(0, 1).toUpperCase();
+  const restOfStr = string.slice(1, string.length);
+  const capitalizeStr = firstCapLtr.concat(restOfStr);
+console.log(capitalizeStr);
+
+  return capitalizeStr;
+}
+
+function reverseString(string){
+const arrayOfStr = [];
+arrayOfStr
+
+
+}
+
+
+
+
+
+export { capitalize, reverseString};
