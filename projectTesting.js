1b81e49315098ce02a65575cabaf553311c05d42
Rohit <rohitjames77@outlook.com>
2025-01-26 09:43:54 +0530

    Add calculator object that contains functions for basic arithmatic

--- projectTesting.js
+++ projectTesting.js
@@ -1,30 +1,36 @@
+
+
+
+
+
 function capitalize(string) {
-  const firstCapLtr = string.slice(0, 1).toUpperCase();
-  const restOfStr = string.slice(1, string.length);
-  const capitalizeStr = firstCapLtr.concat(restOfStr);
-
-  return capitalizeStr;
-}
-
-function reverseString(str) {
-  return str.split("").reverse().join("");
-}
-
-// const calculator = {
-// add:()=>{
-
-// },
-// subtract:()=>{
-
-// },
-// multiply:()=>{
-
-// },
-// divide:()=>{
-
-// }
-// };
-
-
-
-export { capitalize, reverseString };
+    const firstCapLtr = string.slice(0, 1).toUpperCase();
+    const restOfStr = string.slice(1, string.length);
+    const capitalizeStr = firstCapLtr.concat(restOfStr);
+  
+    return capitalizeStr;
+  }
+  
+  function reverseString(str) {
+    return str.split("").reverse().join("");
+  }
+  
+  const calculator = {
+  add:(num1,num2)=>{
+  return num1+num2;
+  },
+  subtract:(num1,num2)=>{
+  return num1 - num2;
+  },
+  multiply:(num1,num2)=>{
+  return num1 * num2;
+  },
+  divide:(num1,num2)=>{
+  return num1%num2;
+  }
+  };
+  
+  
+  
+  export { capitalize, reverseString ,calculator};
+  
