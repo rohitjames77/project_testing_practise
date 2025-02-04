251eba0cbc0075895ad788bac690f5888698ca79
rohit <rohitjames77@outlook.com>
2025-02-04 18:32:12 +0530

    Complete ceaserCipherFunction till encrytion

--- ceaserCipher.js
+++ ceaserCipher.js
@@ -1,12 +1,57 @@
+function ceaserCipher(string, shiftFactor) {
+    const destructureStr = destructureString(string);
+    const shiftedArr = conditionalShiftedStr(destructureStr, shiftFactor);
+    console.log(shiftedArr);
+    const ConvertShiftedStr = convertingBackToStr(shiftedArr);
+    return ConvertShiftedStr;
+  }
+  
+  function destructureString(string) {
+    const charCodeArr = [];
+    const destructureStrArr = string.split("");
+    destructureStrArr.forEach((element) => {
+      charCodeArr.push(element.charCodeAt());
+    });
+    console.log(charCodeArr);
+    return charCodeArr;
+  }
+  function conditionalShiftedStr(destructureStr, shiftFactor) {
+    console.log(shiftFactor);
+  
+    let shiftedArr = destructureStr.map((element) => {
+      switch (true) {
+        case element === 32:
+          return element;
+        case element === 88:
+          return 88 - 23;
+        case element === 89:
+          return 89 - 24;
+        case element === 90:
+          return 90 - 25;
+        case element === 120:
+          return 120 - 23;
+        case element === 121:
+          return 121 - 24;
+        case element === 122:
+          return 122 - 25;
+        case element >= 65 && element <= 87:
+          return element + shiftFactor;
+        case element >= 97 && element <= 119:
+          return element + shiftFactor;
+         case element <= 64: 
+          return element;  
+      case element >= 123 && element <= 1114111:
+        return element;
+        }
+    });
+    return shiftedArr;
+  }
+  
+  
+  function convertingBackToStr(shiftedArr){
+  const convertedStr = String.fromCharCode(...shiftedArr);
+  console.log(convertedStr);
+    return convertedStr;
+  }
 
-function destructureString (str){
-const charCodeArr =[];
-const destructureStrArr = str.split('');
-destructureStrArr.forEach((element)=>{charCodeArr.push(element.charCodeAt())});
-return charCodeArr;
-}
-
-function ceaserCipher(str,shiftFactor){
-//  destructureString(str);
-}
-export {ceaserCipher};
+  export{ceaserCipher}
