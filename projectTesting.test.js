08546eb0d358465373fc33e5e16d18fa7bf8f701
rohit <rohitjames77@outlook.com>
2025-02-10 09:00:56 +0530

    Add test to analyze array average

--- projectTesting.test.js
+++ projectTesting.test.js
@@ -1,10 +1,10 @@
 import { ceaserCipher } from "./ceaserCipher";
-import { capitalize, reverseString, calculator } from "./projectTesting";
+import { capitalize, reverseString, calculator, analyzeArray } from "./projectTesting";
 
 test("first letter to be capital in string", () => {
   expect(capitalize("banku")).toMatch(/^[A-Z][a-zA-Z\s]*$/);
 });
-
+// Second Task : Reverse string ..................
 test("return value to be a string ", () => {
   expect(typeof reverseString("rohit")).toBe("string");
 });
@@ -16,6 +16,8 @@ test("first char of reversed string matches last char of input", () => {
 test("Second char of returned string to be second last char of passed string", () => {
   expect(reverseString("rohit")[1]).toBe("rohit"["rohit".length - 2]);
 });
+
+// Third task : Calculator object .....................
 
 test("Objects first function should return sum of two numbers", () => {
   expect(calculator.add(1, 2)).toBe(3);
@@ -32,6 +34,8 @@ test("Objects third function should return product of two given numbers", () => 
 test("Objects fourth function should return quotient of two given numbers", () => {
   expect(calculator.divide(2, 2)).toBe(0);
 });
+
+// Fourth Task : CeaserCipher Encryption...........................
 
 test('should shift each letter by the given factor', () => {
   expect(ceaserCipher('abc', 3)).toBe('def');
@@ -57,3 +61,13 @@ test('should wrap from Z to A', () => {
 
 test('should handle mixed case with special characters', () => {
   expect(ceaserCipher('aXC !', 3)).toBe('dAF !')});
+
+// Fifth Task : Analyze Array ................
+
+test ('First key should return average of the array',()=>{
+  expect(analyzeArray([1,8,3,4,2,6]).average).toBe(4);
+})
+
+test('Second key should return minimum number of the array',()=>{
+  expect(analyzeArray([1,8,3,4,2,6]).min).toBe(1);
+})
