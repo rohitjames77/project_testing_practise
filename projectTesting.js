75266d5b995d908bb2f57d80aa06f376089d0248
rohit <rohitjames77@outlook.com>
2025-02-10 09:03:44 +0530

    Complete average value of analyzeArray function

--- projectTesting.js
+++ projectTesting.js
@@ -1,36 +1,49 @@
-
-
-
-
-
 function capitalize(string) {
-    const firstCapLtr = string.slice(0, 1).toUpperCase();
-    const restOfStr = string.slice(1, string.length);
-    const capitalizeStr = firstCapLtr.concat(restOfStr);
-  
-    return capitalizeStr;
-  }
-  
-  function reverseString(str) {
-    return str.split("").reverse().join("");
-  }
-  
-  const calculator = {
-  add:(num1,num2)=>{
-  return num1+num2;
+  const firstCapLtr = string.slice(0, 1).toUpperCase();
+  const restOfStr = string.slice(1, string.length);
+  const capitalizeStr = firstCapLtr.concat(restOfStr);
+
+  return capitalizeStr;
+}
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+const calculator = {
+  add: (num1, num2) => {
+    return num1 + num2;
   },
-  subtract:(num1,num2)=>{
-  return num1 - num2;
+  subtract: (num1, num2) => {
+    return num1 - num2;
   },
-  multiply:(num1,num2)=>{
-  return num1 * num2;
+  multiply: (num1, num2) => {
+    return num1 * num2;
   },
-  divide:(num1,num2)=>{
-  return num1%num2;
-  }
+  divide: (num1, num2) => {
+    return num1 % num2;
+  },
+};
+
+function analyzeArray(array) {
+  const arrAverage = analyzeAverage(array);
+  const arrMin= analyzeMinimum(array);  
+  const object = {
+    average: arrAverage,
   };
-  
-  
-  
-  export { capitalize, reverseString ,calculator};
-  
+  return object;
+}
+//Helper function to analyze average .......................
+function analyzeAverage(array) {
+  console.log(array.length);
+
+  const average = array.at(array.length / 2);
+  console.log(average);
+
+  return average;
+}
+function analyzeMinimum(array){
+
+}
+
+export { analyzeArray, capitalize, reverseString, calculator };
